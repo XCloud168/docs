7e154a8ba66812e8e671455bc03e315c101bb4ff
Woods <woods.web3@gmail.com>
2025-11-24 18:54:48 +0800

    fix:修复错误链接

--- vocs.config.tsx
+++ vocs.config.tsx
@@ -58,7 +58,7 @@ export default defineConfig({
   topNav: [
     {
       text: "Geting Started",
-      link: "/docs/getting-started",
+      link: "/docs/tutorials",
     },
     // {
     //   text: "Blog",
